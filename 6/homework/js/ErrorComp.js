c8b91b04bc3a5c75832b201542597aa95475b8fc
Kirill Gorshkoff <gobzter@gmail.com>
2020-09-07 20:34:14 +0300

    done homework

---
+++ 6/homework/js/ErrorComp.js
@@ -0,0 +1,12 @@
+// Vue.component('error', {
+//     data() {
+//         return {}
+//     },
+//     methods: {},
+//     template: `<form action="#" class="search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
+//                     <input type="text" class="search-field" v-model="userSearch">
+//                     <button class="btn-search" type="submit">
+//                         <i class="fas fa-search"></i>
+//                     </button>
+//                 </form>`
+// });
